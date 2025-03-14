caea3886bf1a61ac2afaeb3ac9747effe2bcc091
Stuart <40066813+stuart-coles1111@users.noreply.github.com>
2025-03-14 14:48:30 +0100

    horse race results

---
+++ R/horse_race_results.R
@@ -0,0 +1,12 @@
+#' Horse race results for Activity 8
+#'
+#' Horse race results for Activity 8 of Playing With Statistics
+#'
+#' @format
+#' A data frame with 50 rows and 7 columns
+#' @source {in-house}
+#' @examples
+#' horse_race_results
+#'
+#'
+"horse_race_results"
