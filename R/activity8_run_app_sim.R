4638694c2f3781b2c0ac23adcef239d69bc2c262
Stuart <40066813+stuart-coles1111@users.noreply.github.com>
2025-03-25 16:58:23 +0100

    activity 8 sim version

---
+++ R/activity8_run_app_sim.R
@@ -0,0 +1,11 @@
+#' Run Horse Race Simulation App
+#' Executes simulation version of horse race app for Activity 8 in Playing With Statistics
+#'
+#' @examples
+#' activity8_run_app_sim()
+#'
+#' @export
+#'
+activity8_run_app_sim <- function() {
+    shiny::runApp("~/pws/inst/shiny/app_horse_race_game_sim.R")
+}
