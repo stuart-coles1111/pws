e12abfb592019cb3d039e0163fa890a3e5a58f03
Stuart <40066813+stuart-coles1111@users.noreply.github.com>
2025-12-03 23:24:03 +0100

    trick stuff

--- R/multi_trick.R
+++ R/multi_trick.R
@@ -4,6 +4,7 @@
 #'
 #' @param nrep number of repetitions
 #' @param picture_value score to give picture cards
+#' @param seed value of seed for random number generator
 #'
 #' @returns  summary statistics from multiple trick runs
 #' @examples multi_trick()
@@ -13,11 +14,12 @@
 #'
 #'
 
-multi_trick <- function(nrep = 10000, picture_value = 10){
+multi_trick <- function(nrep = 10000, picture_value = 10, seed = NULL){
+    if(!is.null(seed)) set.seed(seed)
     trick_agree <- lapply(1:nrep, function(x, picture_value) single_trick(picture_value, show_result = FALSE), picture_value = picture_value)
     trick_agree <- trick_agree %>% unlist %>% table
-    names(trick_agree) <- c("disagree", "agree")
+    names(trick_agree) <- c("incorrect", "correct")
     p <- trick_agree[1]/nrep %>% as.numeric
     names(p) <- "p"
-    list(nrep = nrep, picture_value = picture_value, results=trick_agree, fail_prob=p)
+    list(nrep = nrep, results=trick_agree, fail_prob=p)
 }
---
+++ R/trick_fail_prob_se.R
@@ -0,0 +1,23 @@
+#' Trick failure probability SE
+#'
+#' Standard error calculation of trick failure probability for Activity 5
+#'
+#' @param trick_res output from multi_trick
+#' @param n_resample number of resamples
+#' @param seed value of seed for random number generator
+#'
+#' @returns  value of standard error
+#' @examples trick_res = multi_trick()
+#'           trick_fail_prob_se(trick_res)
+#'
+#' @export
+#'
+#'
+#'
+#'
+
+trick_fail_prob_se <- function(trick_res, n_resample = 1000, seed = NULL){
+    if(!is.null(seed)) set.seed(seed)
+    resample <- rbinom(n_resample, trick_res$nrep, trick_res$fail_prob) / trick_res$nrep
+    sd(resample)
+}
