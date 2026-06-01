b40174eb35acbeda97f1d9a2987c801680540a6f
Stuart <40066813+stuart-coles1111@users.noreply.github.com>
2026-06-01 12:00:27 +0200

    app8 run

--- R/activity8_run_app.R
+++ R/activity8_run_app.R
@@ -1,26 +1,34 @@
-#' Run Horse Race App
-#' @param mode "multiplayer" or "singleplayer"
-#'
+#' Run Activity 8 app
 #' @export
-activity8_run_app <- function(mode = c("multiplayer", "singleplayer")) {
+activity8_run_app <- function(
+        commission_max = 0.2,
+        bet_rate = 5,
+        bet_proportion = 0.1,
+        pool_init = 100,
+        nominal_stake = 100
+) {
 
-    mode <- match.arg(mode)
-
-    app_file <- switch(
-        mode,
-        multiplayer = "app_activity8.R",
-        singleplayer = "app_activity8_sim.R"
+    appDir <- system.file(
+        "shiny/app_activity8_dev2.R",
+        package = "pws"
     )
 
-    appDir <- system.file("shiny", app_file, package = "pws")
+    env <- new.env(parent = globalenv())
 
-    if (appDir == "") {
-        stop(
-            paste0("Could not find app file: ", app_file,
-                   ". Try reinstalling the package."),
-            call. = FALSE
+    env$commission_max <- commission_max
+    env$bet_rate <- bet_rate
+    env$bet_proportion <- bet_proportion
+    env$pool_init <- pool_init
+    env$nominal_stake <- nominal_stake
+
+    # source app
+    source(appDir, local = env)
+
+    # 🚨 THIS forces launch
+    shiny::runApp(
+        list(
+            ui = env$ui,
+            server = env$server
         )
-    }
-
-    shiny::runApp(appDir, display.mode = "normal")
+    )
 }
