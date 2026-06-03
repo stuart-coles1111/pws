0de20e9682bfd19feb6778ba5a7e99b3ce6b12e3
Stuart <40066813+stuart-coles1111@users.noreply.github.com>
2026-06-03 22:00:24 +0200

    Update chapter8.R

--- inst/shiny/stats_lab/chapters/chapter8.R
+++ inst/shiny/stats_lab/chapters/chapter8.R
@@ -105,42 +105,6 @@ league_sim <- function(df, tau) {
           arrange(df_out, desc(tot), desc(gd), desc(gf))[[1]])
 }
 
-# =========================================================
-# DYNAMIC SIMULATION
-# =========================================================
-
-dynamic_league_sim <- function(df, schedule, tau, sigma = 0.1) {
-
-    results <- dynamic_season_sim(
-        df,
-        schedule[,1],
-        schedule[,2],
-        schedule[,3],
-        tau,
-        sigma = sigma
-    )
-
-    points <- schedule
-    points$hp <- results[[1]]
-    points$ap <- results[[2]]
-    points$gd <- results[[3]]
-    points$gh <- results[[4]]
-    points$ga <- results[[5]]
-
-    df_h <- points %>%
-        group_by(Home.Team) %>%
-        summarise(tot = sum(hp), gd = sum(gd), gf = sum(gh), .groups = "drop")
-
-    df_a <- points %>%
-        group_by(Away.Team) %>%
-        summarise(tot = sum(ap), gd = -sum(gd), gf = sum(ga), .groups = "drop")
-
-    df_out <- df_h
-    df_out[,-1] <- df_h[,-1] + df_a[,-1]
-
-    match(df$teams,
-          arrange(df_out, desc(tot), desc(gd), desc(gf))[[1]])
-}
 
 # =========================================================
 # UI
@@ -332,16 +296,32 @@ chapter8_server <- function(id) {
 
             sims <- sapply(
                 1:input$n_sim,
-                function(x) league_sim(df24, PL24_pars$tau)
+                function(x) league_sim(PL24_pars$teams, PL24_pars$tau)
             )
 
-            rownames(sims) <- df24$teams
+            rownames(sims) <- PL24_pars$teams$teams
             sims
         })
 
         # -------------------------
         # DYNAMIC SIM
         # -------------------------
+
+        dynamic_season_sim <- function(df, round, team_h,team_a, tau, ro=0.9, sigma = .1){
+            teams_dynamic <- make_dynamic(PL24_pars$teams, ro=ro, sigma = sigma)
+            mu_h <- mu_a <- c()
+
+            for(i in 1:length(round)){
+                mu_h <- c(mu_h, exp(tau + teams_dynamic[match(team_h[i], df$teams), round[i]+7] - teams_dynamic[match(team_a[i], df$teams), round[i]+45]))
+                mu_a <- c(mu_a, exp(teams_dynamic[match(team_a, df$teams), round[i]+7] - teams_dynamic[match(team_h[i], df$teams), round[i]+45]))
+            }
+
+            g_h <- rpois(length(team_h), mu_h)
+            g_a <- rpois(length(team_a), mu_a)
+            p_h <- ifelse(g_h > g_a, 3, ifelse(g_h == g_a, 1, 0))
+            p_a <- ifelse(g_h > g_a, 0, ifelse(g_h == g_a, 1, 3))
+            list(p_h, p_a, g_h - g_a, g_h, g_a)
+        }
 
         dynamic_sim <- eventReactive(input$run_dynamic, {
 
@@ -353,16 +333,67 @@ chapter8_server <- function(id) {
                 1:input$n_sim,
                 function(x)
                     dynamic_league_sim(
-                        df24,
+                        PL24_pars$teams,
                         schedule,
                         PL24_pars$tau,
                         input$sigma
                     )
             )
 
-            rownames(sims) <- df24$teams
+            rownames(sims) <- PL24_pars$teams$teams
             sims
         })
+
+        make_dynamic <- function(teams, ro=0.9, sigma=.1){
+            teams_dynamic <- teams
+            teams_dynamic <- cbind(teams_dynamic, matrix(0, nr=20, nc= 76))
+            colnames(teams_dynamic)[4:41] <- paste0("a_round_",1:38)
+            colnames(teams_dynamic)[42:79] <- paste0("b_round_",1:38)
+            for(i in 1:20){
+                td <- mvrnorm(37, c(0,0), matrix(c(1,ro,ro,1), nr=2)*sigma^2)
+                td <- rbind(c(0,0),td)
+                ad <- teams[i,"alpha"] + cumsum(td[,1])
+                bd <- teams[i,"beta"] + cumsum(td[,2])
+                teams_dynamic[i, 8:45] <- ad
+                teams_dynamic[i, 46:83] <- bd
+            }
+            teams_dynamic
+        }
+
+
+        dynamic_league_sim <- function(df, schedule, tau, sigma = 0.1) {
+
+            results <- dynamic_season_sim(
+                df,
+                schedule[,1],
+                schedule[,2],
+                schedule[,3],
+                tau,
+                sigma = sigma
+            )
+
+            points <- schedule
+            points$hp <- results[[1]]
+            points$ap <- results[[2]]
+            points$gd <- results[[3]]
+            points$gh <- results[[4]]
+            points$ga <- results[[5]]
+
+            df_h <- points %>%
+                group_by(Home.Team) %>%
+                summarise(tot = sum(hp), gd = sum(gd), gf = sum(gh), .groups = "drop")
+
+            df_a <- points %>%
+                group_by(Away.Team) %>%
+                summarise(tot = sum(ap), gd = -sum(gd), gf = sum(ga), .groups = "drop")
+
+            df_out <- df_h
+            df_out[,-1] <- df_h[,-1] + df_a[,-1]
+
+            match(df$teams,
+                  arrange(df_out, desc(tot), desc(gd), desc(gf))[[1]])
+        }
+
 
         # -------------------------
         # PLOTS
@@ -370,12 +401,12 @@ chapter8_server <- function(id) {
 
         output$static_plot <- renderPlot({
             req(static_sim())
-            league_position_plot(df24, static_sim(), 4)
+            league_position_plot(PL24_pars$teams, static_sim(), 4)
         })
 
         output$dynamic_plot <- renderPlot({
             req(dynamic_sim())
-            league_position_plot(df24, dynamic_sim(), 4)
+            league_position_plot(PL24_pars$teams, dynamic_sim(), 4)
         })
 
         output$comparison_plot <- renderPlot({
