0c363447a0bb9cad754c38d5875adeef69fb4bd8
Stuart <40066813+stuart-coles1111@users.noreply.github.com>
2026-06-23 13:59:57 +0200

    Update launch_activity.R

--- R/launch_activity.R
+++ R/launch_activity.R
@@ -1,82 +1,55 @@
+wait_for_port <- function(port, timeout = 10) {
+
+    start <- Sys.time()
+    url <- sprintf("http://127.0.0.1:%s", port)
+
+    repeat {
+
+        ok <- tryCatch(
+            {
+                httr::GET(url, httr::timeout(0.5))
+                TRUE
+            },
+            error = function(e) FALSE
+        )
+
+        if (ok) return(TRUE)
+
+        if (difftime(Sys.time(), start, units = "secs") > timeout) {
+            return(FALSE)
+        }
+
+        Sys.sleep(0.05)
+    }
+}
+
 launch_activity <- function(app_name) {
 
-    appdir <- system.file(
-        "shiny",
-        app_name,
-        package = "pws"
-    )
-
+    appdir <- system.file("shiny", app_name, package = "pws")
     port <- httpuv::randomPort()
 
     p <- callr::r_bg(
         function(appdir, port) {
-
-            message("Starting app")
-            message(appdir)
-            message(port)
-
             shiny::runApp(
                 appdir,
                 host = "127.0.0.1",
                 port = port,
                 launch.browser = FALSE
             )
-
         },
-        args = list(appdir, port),
-        stdout = "NULL",
-        stderr = "NULL",
-        supervise = FALSE
+        args = list(appdir = appdir, port = port),
+        stdout = NULL,
+        stderr = NULL,
+        supervise = TRUE
     )
 
-    Sys.sleep(1)
+    ok <- wait_for_port(port, timeout = 10)
 
-    if (!p$is_alive()) {
-
-        cat(
-            paste(p$read_error_lines(), collapse = "\n"),
-            "\n"
-        )
-
-        cat(
-            paste(p$read_output_lines(), collapse = "\n"),
-            "\n"
-        )
+    if (!ok) {
+        stop("App failed to start (port not available).")
     }
 
-    url <- sprintf("http://127.0.0.1:%s", port)
-
-    deadline <- Sys.time() + 15
-
-    repeat {
-
-        ready <- tryCatch({
-
-            res <- httr::GET(
-                url,
-                httr::timeout(1)
-            )
-
-            httr::status_code(res) == 200
-
-        }, error = function(e) FALSE)
-
-        if (ready)
-            break
-
-        if (!p$is_alive()) {
-            stop("App process terminated before starting.")
-        }
-
-        if (Sys.time() > deadline) {
-            stop("Timed out waiting for app to start.")
-        }
-
-        Sys.sleep(0.1)
-    }
-
-
-    utils::browseURL(url)
+    utils::browseURL(sprintf("http://127.0.0.1:%s", port))
 
     invisible(p)
 }
